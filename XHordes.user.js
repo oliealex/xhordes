927fd478926fb706b53f35284e4c53a1043cbd0e
Campinator <joefakocamp@gmail.com>
2017-11-26 03:37:59 -0500

    sorry it's late

--- XHordes.user.js
+++ XHordes.user.js
@@ -17,7 +17,7 @@
         url: "http://hordes.io",
         onload: function(ev) {
             document.open();
-            let index = ev.responseText.replace('<script src="script/dist.min.js"></script>', '<script src="https://cdn.rawgit.com/LegusX/xhordes/fd915da2/src/PvP/PvP0-1.js"></script> ');
+            let index = ev.responseText.replace('<script src="script/dist.min.js"></script>', '<script src="https://cdn.rawgit.com/LegusX/xhordes/c366b242/src/PvP/PvP0-2.js"></script> ');
             index = index.replace('<link rel="stylesheet" href="gamestyles.css">', '<link rel="stylesheet" href="https://cdn.rawgit.com/LegusX/xhordes/fd915da2/src/PvP/PvP0-1.css">');
             document.write(index);
             document.close();
