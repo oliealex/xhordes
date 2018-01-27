cafa8e6ac508eb82ee3fd335dbd66b60515333a2
oliealex <alexander_racho@hotmail.com>
2018-01-27 20:02:10 +0100

    test

--- src/levelup/levelup2.js
+++ src/levelup/levelup2.js
@@ -15550,7 +15550,7 @@ else window.xhordes.filter = JSON.parse(localStorage.filter);
     h.prototype.tick = function(t) {
         if (!this.stats.isDead()) {
             var e = new THREE.Vector3(0, 0, 0);
-            if (e.z = -this.walkForward, e.x = this.walkSideward, e.lengthSq() > 0 && e.normalize(), -1 == this.walkForward && (e.z *= .5, e.x *= .5), e.applyAxisAngle(new THREE.Vector3(0, 1, 0), this.rotation), this.velocity = e, !this.ignSteep && !this.isAi && (Wt || this == Pi) && (0 != this.velocity.x || 0 != this.velocity.y)) {
+            if (e.z = -this.walkForward, e.x = this.walkSideward, e.lengthSq() > 0 && e.normalize(), -1 == this.walkForward && (e.z *= .5, e.x *= .5), e.applyAxisAngle(new THREE.Vector3(0, 1, 0), this.rotation), this.velocity = e * 10, !this.ignSteep && !this.isAi && (Wt || this == Pi) && (0 != this.velocity.x || 0 != this.velocity.y)) {
                 var i = e.clone();
                 i.normalize();
                 var n = Hi(this.position.x, this.position.z),
